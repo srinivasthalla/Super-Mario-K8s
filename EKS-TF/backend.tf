7dd6bbcec59e74499beb20d33f99d3c9dbe4c4ab
Srinivas Thalla <157224356+srinivasthalla@users.noreply.github.com>
2025-11-08 18:44:30 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "amn-s3-demo-bucket-31051995"      # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
